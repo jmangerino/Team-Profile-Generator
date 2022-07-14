40735d0b46e0d364adafa8c3c53e1fc27f593c20
Josh Man <joshuamangerino@gmail.com>
2022-07-14 13:58:33 -0600

    added prompts

---
+++ index.js
@@ -0,0 +1,107 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateTeam = require('./src/template');
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+
+const teamArray = [];
+
+const addManager = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the manager's name?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is manager's id?"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the manager's email?"
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is the mangager's office number?"
+        },
+    ])
+    .then(managerInput => {
+        const { name, id, email, officeNumber } = managerInput;
+        const manager = new Manager (name, id, email, officeNumber);
+        teamArray.push(manager);
+    })
+};
+
+const addEmployee = () => {
+    console.log('Adding employee');
+    
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What employee would you like to add next?',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is your employee's name"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the employee's id?"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is your employee's email?"
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is your Employee's github username?",
+            when: (input) => input.role === 'Engineer'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What is your intern's school?",
+            when: (input) => input.role === 'Intern'
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: "Would you like to add any more employee's?"
+        },
+    ])
+    .then(employeeData => {
+        let { name, id, email, role, github, school, confirmAddEmployee } = employeeData;
+        let employee;
+
+        if (role === 'Engineer') {
+            employee = new Engineer (name, id, email, github);
+            console.log(employee);
+        } else if (role === 'Intern') {
+            employee = new Intern (name, id, email, school);
+            console.log(employee);
+        }
+
+        teamArray.push(employee);
+
+        if (confirmAddEmployee) {
+            return addEmployee(teamArray);
+        } else {
+            return teamArray
+        }
+    })
+};
+
